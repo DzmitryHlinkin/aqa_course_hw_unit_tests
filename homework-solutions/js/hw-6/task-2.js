e0c42a2f7868883d611cad4c9c363a1f61e0c0b7
DzmitryHlinkin <74535370+DzmitryHlinkin@users.noreply.github.com>
2025-02-24 20:27:13 +0300

    hw 6 second part

--- homework-solutions/js/hw-6/task-2.js
+++ homework-solutions/js/hw-6/task-2.js
@@ -13,7 +13,42 @@
   const myPizzasT2 = ['Peperoni', 'Caprichosa', 'Diablo', '4 cheeses', 'hawai'];
 */
 
-let resultUnique;
-let resultNull;
+let resultUnique = [];
+let resultNull = [];
+let nikomuNeNugnaiaPeremennaia =[]
+const competitorPizzas = ['Peperoni', 'Caprichosa', 'Diablo', '4 cheeses', 'hawai'];
+const myPizzasT1 = ['Peperoni', 'Margherita', 'Diablo', 'Vegetarian'];
+const myPizzasT2 = ['Peperoni', 'Caprichosa', 'Diablo', '4 cheeses', 'hawai'];
+const ToLowerMyPizzas = myPizzasT1.map(Element => Element.toLowerCase())
+const ToLowerCompetitorPizzas = competitorPizzas.map(Element => Element.toLocaleLowerCase())
+for (const pizzas of ToLowerMyPizzas) {
+  
+  if (ToLowerCompetitorPizzas.includes(pizzas)) 
+  {
+    ++nikomuNeNugnaiaPeremennaia
+    if (nikomuNeNugnaiaPeremennaia === ToLowerCompetitorPizzas.length){
+    resultNull = null}
+  }
 
+  else {
+  resultUnique.push(pizzas)}
+}  
+
+//второй прогон с другими условиями
+const ToLowerMyPizzas2 = myPizzasT2.map(Element => Element.toLowerCase())
+
+for (const pizzas of ToLowerMyPizzas2) {
+  
+  if (ToLowerCompetitorPizzas.includes(pizzas)) 
+  {
+    ++nikomuNeNugnaiaPeremennaia
+    if (nikomuNeNugnaiaPeremennaia === ToLowerCompetitorPizzas.length){
+    resultNull = null}
+  }
+
+  else {
+  resultUnique.push(pizzas)}
+}  
+console.log(resultNull)
+console.log(resultUnique)
 export { resultNull, resultUnique };
